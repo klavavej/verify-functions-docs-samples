de51359af3a2d4697a5633b4012ca8859c79c7cc
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2021-12-15 10:26:19 -0800

    Update send-pdf-background.js

--- netlify/functions/send-pdf-background.js
+++ netlify/functions/send-pdf-background.js
@@ -15,12 +15,23 @@ exports.handler = async function (event) {
   const { content, destination } = JSON.parse(event.body);
   console.log(`Sending PDF report to ${destination}`);
 
+  const report = Buffer.from(
+    new jsPDF().text(content, 10, 10).output('arraybuffer'),
+  );
   const info = await transporter.sendMail({
     from: process.env.MAILGUN_SENDER,
     to: destination,
     subject: 'Your report is ready!',
     text: 'See attached report PDF',
+    attachments: [
+      {
+        filename: `report-${new Date().toDateString()}.pdf`,
+        content: report,
+        contentType: 'application/pdf',
+      },
+    ],
   });
 
-  console.log(`content: ${content}`);
+  console.log(`PDF report sent: ${info.messageId}`);
 };
+
